ebc6f732b87f0a6f05f522661acaf3a3600505b1
Zak Bowyer <zak.bowyer@live.co.uk>
2015-04-05 15:38:35 +0100

    script dialogue functionality (no image)

--- WebContent/src/Dialog.js
+++ WebContent/src/Dialog.js
@@ -42,6 +42,9 @@ function Dialog() {
 	}
 	
 	this.createImage = function(src) {
+		if (src == null) {
+			return null;
+		}
 		var img = src;
 		var thisImage = new createjs.Bitmap(img);
 		thisImage.scaleX = (DIALOG_WIDTH) /img.width;
@@ -55,9 +58,9 @@ function Dialog() {
 		console.log(src);
 		var txt = new createjs.Text(src, "20px Arial", "#ffffff");
 		txt.textBaseline = "alphabetic";
-		txt.y = image == null ? 0 : (image.image.height * image.scaleY) * (9/16);
+		txt.y = image == null ? stage.canvas.height - 320 : (image.image.height * image.scaleY) * (9/16);
 		txt.x = image == null? 0 : (image.image.width * image.scaleX);
-		txt.lineWidth = stage.canvas.width - 425;
+		txt.lineWidth = image == null ? stage.canvas.width - 20 : stage.canvas.width - 425;
 		
 		return txt;
 	}
@@ -72,11 +75,26 @@ function Dialog() {
 		return txt;
 	}
 	
-	this.createBackground = function() {
+	this.createBackground = function(includeName) {
 		var container = new createjs.Container();
-		var nameBackground = drawBorderedRectangle(stage.canvas.width - 600, 50, 180, stage.canvas.height - 400, "#FFFFFF");
-		var textBackground = drawBorderedRectangle(stage.canvas.width - 220, 170, 180, stage.canvas.height - 350, "#FFFFFF");
-		container.addChild(nameBackground);
+		if (includeName) {
+			var nameBackground = drawBorderedRectangle(stage.canvas.width - 600, 50, 180, stage.canvas.height - 400, "#FFFFFF");
+			container.addChild(nameBackground);
+		}
+		var txtBackgroundX;
+		var txtBackgroundWidth;
+		var txtBackgroundHeight;
+		if (image == null) {
+			txtBackgroundX = -15;
+			txtBackgroundWidth = stage.canvas.width + 10;
+			txtBackgroundHeight = 190;
+		}
+		else {
+			txtBackgroundX = 180;
+			txtBackgroundWidth = stage.canvas.width - 220;
+			txtBackgroundHeight = 170;
+		}
+		var textBackground = drawBorderedRectangle(txtBackgroundWidth, txtBackgroundHeight, txtBackgroundX, stage.canvas.height - 350, "#FFFFFF");
 		container.addChild(textBackground);
 		return container;
 	}
@@ -87,8 +105,8 @@ function Dialog() {
 		var charName = CHARACTER_NAMES[dialog.character];
 		
 		container = new createjs.Container();
-		var background = this.createBackground();
 		image = this.createImage(dialogImage);
+		var background = this.createBackground(charName != null);
 		text = this.createText(dialog.text);
 		name = this.createName(charName);
 		container.addChild(background);
