ffac01d709281469a584895d588cc06bd3b81162
Zak Bowyer <zak.bowyer@live.co.uk>
2015-04-09 13:06:14 +0100

    clean up: menu refactor (2)

--- WebContent/src/menu/Menu.js
+++ WebContent/src/menu/Menu.js
@@ -1,12 +1,12 @@
 function initMenu() {
 	var menuBar = new createjs.Container();
-	var menuIconDimensions = {"width": stage.canvas.width / MENU_ICONS};
+	var menuIconDimensions = {"width": stage.canvas.width / MENU_ICONS, "height": MENU_HEIGHT};
 	var current = {"width": 0};
 	
 	var icons = [MENU_INVENTORY, MENU_PARTY, MENU_SAVE];
 	
 	for (var i = 0; i < icons.length; i++) {
-		var icon = new MenuIcon(icons[i], menuIconDimensions.width, MENU_HEIGHT);
+		var icon = new MenuIcon(icons[i], menuIconDimensions);
 		icon.x = current.width;
 		icon.y = 0;
 		menuBar.addChild(icon);
--- WebContent/src/menu/MenuIcon.js
+++ WebContent/src/menu/MenuIcon.js
@@ -1,9 +1,9 @@
-function MenuIcon(name, width, height) {
+function MenuIcon(name, iconDimensions) {
 	var menu;
 	var state = {"open": false};
 
 	var shape = new createjs.Shape();	
-	var dimensions = {"width": width, "height": stage.canvas.height / 2};
+	var menuDimensions = {"width": iconDimensions.width, "height": stage.canvas.height / 2};
 	
 	function clickMenuIcon() {
 		if (!state.open) {
@@ -27,11 +27,11 @@ function MenuIcon(name, width, height) {
 	}
 	
 	if (name === MENU_INVENTORY) {
-		shape.graphics.beginFill("green").drawRect(0, 0, width, height);
+		shape.graphics.beginFill("green").drawRect(0, 0, iconDimensions.width, iconDimensions.height);
 		shape.addEventListener("click", function() {
 			if (priority <= MENU_PRIORITY) {
 				if (menu == null) {
-					menu = new MenuInventory(dimensions);
+					menu = new MenuInventory(menuDimensions);
 				}
 				clickMenuIcon();
 				stage.update();
@@ -40,17 +40,17 @@ function MenuIcon(name, width, height) {
 	}
 	
 	if (name === MENU_PARTY) {
-		shape.graphics.beginFill("red").drawRect(0, 0, width, height);
+		shape.graphics.beginFill("red").drawRect(0, 0, iconDimensions.width, iconDimensions.height);
 		shape.addEventListener("click", function() {
 			if (priority <= MENU_PRIORITY) {
-				stage.addChild(new MenuParty(dimensions));
+				stage.addChild(new MenuParty(menuDimensions));
 				stage.update();
 			}
 		});
 	}
 	
 	if (name === MENU_SAVE) {
-		shape.graphics.beginFill("blue").drawRect(0, 0, width, height);
+		shape.graphics.beginFill("blue").drawRect(0, 0, iconDimensions.width, iconDimensions.height);
 		shape.addEventListener("click", function() {
 			if (priority <= MENU_PRIORITY) {
 				stage.update();
