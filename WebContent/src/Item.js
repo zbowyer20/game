85e59c28ee90a4b78c0ba98bb2451579204fad51
Zak Bowyer <zak.bowyer@live.co.uk>
2015-04-09 13:09:23 +0100

    clean up: Item.js

--- WebContent/src/Item.js
+++ WebContent/src/Item.js
@@ -1,9 +1,4 @@
 function Item(id, name, description, inventoryImage) {
-	var id;
-	var name;
-	var description;
-	var inventoryImage;
-	
 	this.id = id;
 	this.name = name;
 	this.description = description;
